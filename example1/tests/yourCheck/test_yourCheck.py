e3c40155edbaf47376d38b5569f2d04cc99ef5ea
aartola <aartola@veinteractive.com>
2021-06-07 10:32:56 +0200

    jenkins example

--- example1/tests/yourCheck/test_yourCheck.py
+++ example1/tests/yourCheck/test_yourCheck.py
@@ -8,6 +8,6 @@ import pytest
 
 def test_yourCheck():
     platform = { 'a': 1, 'b': 2 }
-    print ("Your check test")
+    print ("Your check test one")
     one, two = yourCheck.checkyourPlatform (platform)
     assert one == 1, two ==2
