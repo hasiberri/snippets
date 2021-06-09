7560e78382df23c17446ca3b9b3fb36c0add807e
aartola <aartola@veinteractive.com>
2021-06-09 16:39:12 +0200

    testing jenkins

--- vars/pythonDocker.groovy
+++ vars/pythonDocker.groovy
@@ -66,7 +66,7 @@ spec:
             steps {
                 container('kaniko') {
                   dir("${image}/"){
-			sh '/kaniko/executor -f Dockerfile --context=dir://workspace --no-push'
+			sh '/kaniko/executor -f ./Dockerfile --context=./ --no-push'
                   }
                 }
             }
