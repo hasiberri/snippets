e6ef1a7dd33a2f55c54818afd5b727aadadb7ba3
aartola <aartola@veinteractive.com>
2021-06-27 19:56:29 +0200

    testing jenkins

--- example1/README.md
+++ example1/README.md
@@ -1,2 +1,2 @@
-# This is an example of staging full again again
+# This is an example of staging full again
 
--- vars/pythonDocker.groovy
+++ vars/pythonDocker.groovy
@@ -44,7 +44,7 @@ def call(image) {
           steps {
             container('kaniko') {
               dir("${image}/"){
-			          sh '/kaniko/executor -f Dockerfile --context=./ --no-push'
+			          sh '/kaniko/executor -f Dockerfile --context=./ --tarPath=image.tar--no-push'
               }
 	          }
           }
@@ -54,7 +54,7 @@ def call(image) {
           steps {
             container('dockle') {
               dir("${image}/"){
-			          sh 'dockle ${image}'
+			          sh 'dockle --input image.tar'
               }
 	          }
           }
