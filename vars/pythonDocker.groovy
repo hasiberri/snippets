5c0bcd918acd4aefa420c92f3c324d91f4d8faac
aartola <aartola@veinteractive.com>
2021-06-27 19:23:02 +0200

    testing jenkins

--- vars/pythonDocker.groovy
+++ vars/pythonDocker.groovy
@@ -4,7 +4,7 @@ def call(image) {
   pipeline {
     agent {
      kubernetes {
-      label 'mypython'
+      inheritFrom 'mypython'
       defaultContainer 'jnlp'
       yamlFile 'pythonDocker.yaml'
       }
@@ -12,52 +12,52 @@ def call(image) {
 
     stages {
         stage('python Unit Test') {
-            when { changeset "${image}/**"}
-            steps {
-                container('python') {
-		  dir("${image}/"){
-        		sh 'pip install pytest'
-        		sh 'pytest'
-		  }
-		}
-            }
+          when { changeset "${image}/**"}
+          steps {
+            container('python') {
+		          dir("${image}/"){
+        		    sh 'pip install pytest'
+        		    sh 'pytest'
+		          }
+		        }
+          }
         }
         stage('python Security Test') {
-            when { changeset "${image}/**"}
-            steps {
-                container('python') {
-		  dir("${image}/"){
-        		sh 'pip install bandit'
-        		sh '''bandit -r ./src 2>&1 | tee ./out.log
-				num=`grep "Severity: High" ./out.log | wc -l`
-				if [ $num -eq 0 ]; then
-     					exit 0
-				else
-     					exit 1
-				fi'''
-                  }
-	        }
-            }
+          when { changeset "${image}/**"}
+          steps {
+            container('python') {
+		          dir("${image}/"){
+        	      sh 'pip install bandit'
+        		    sh '''bandit -r ./src 2>&1 | tee ./out.log
+				              num=`grep "Severity: High" ./out.log | wc -l`
+				              if [ $num -eq 0 ]; then
+     					          exit 0
+				              else
+     					          exit 1
+				              fi'''
+              }
+	          }
+          }
         }
         stage('Build Image') {
-            when { changeset "${image}/**"}
-            steps {
-                container('kaniko') {
-                  dir("${image}/"){
-			sh '/kaniko/executor -f ./Dockerfile --cache=true --context=./ --destination=${image} --no-push'
-                  }
-                }
-            }
+          when { changeset "${image}/**"}
+          steps {
+            container('kaniko') {
+              dir("${image}/"){
+			          sh '/kaniko/executor -f ./Dockerfile --cache=true --context=./ --destination=${image} --no-push'
+              }
+	          }
+          }
         }
         stage('Lint Image') {
-            when { changeset "${image}/**"}
-            steps {
-                container('dockle') {
-                  dir("${image}/"){
-			sh '/dockle ${image}'
-                  }
-                }
-            }
+          when { changeset "${image}/**"}
+          steps {
+            container('dockle') {
+              dir("${image}/"){
+			          sh '/dockle ${image}'
+              }
+	          }
+          }
         }
 
     }
